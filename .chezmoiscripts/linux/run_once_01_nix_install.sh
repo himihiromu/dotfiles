97dcad8e2daa7de4fad7f93e9f8e794f67627f47
himihiromu <himihiromu@icloud.com>
2026-03-21 04:37:59 +0900

    fix: shell再起動ではなくnixの適用にする

--- .chezmoiscripts/linux/run_once_01_nix_install.sh
+++ .chezmoiscripts/linux/run_once_01_nix_install.sh
@@ -3,5 +3,6 @@
 # Nix のインストール
 if [ ! -d /nix ]; then
   curl -fsSL https://install.determinate.systems/nix | sh -s -- install
+  . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
 fi
-exec $SHELL -l
+
