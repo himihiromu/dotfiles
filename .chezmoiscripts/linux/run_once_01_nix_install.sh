dfeb406a619dc942c793ab553e74edc1e51ec694
himihiromu <himihiromu@icloud.com>
2026-08-17 10:17:57 +0900

    fix: use portable Bash shebangs on NixOS

--- .chezmoiscripts/linux/run_once_01_nix_install.sh
+++ .chezmoiscripts/linux/run_once_01_nix_install.sh
@@ -1,8 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Nix のインストール
 if [ ! -d /nix ]; then
   curl -fsSL https://install.determinate.systems/nix | sh -s -- install
   . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
 fi
-
